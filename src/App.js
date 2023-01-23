a00e31d9b0f857861e9ff39e97cbaca8864aaa14
Trinity <amoahtnt6@gmail.com>
2023-01-23 17:03:09 +0000

    Footer page done

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { Routes, Route } from "react-router-dom";
+import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Projects from "./pages/Projects";
@@ -9,8 +10,9 @@ function App() {
     <Navbar/>
       <Routes>
         <Route path="/" index element={<Home/>}/>
-        <Route path="/project" index element={<Projects/>}/>
+        <Route path="/project" element={<Projects/>}/>
       </Routes>
+      <Footer/>
     </>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import '../style/footer_style.css'
+import { FaLinkedin, FaGithubSquare, FaTwitterSquare } from 'react-icons/fa'
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="footer-first">
+        <h3>Just say hi</h3>
+        <p>I'm always open to discuss your project and talk about new things</p>
+        <div className="footer-first-wrapper">
+          <div className="follow">
+            <p>Mail me at:</p>
+            <h5>amoahtnt6@gmail.com</h5>
+          </div>
+          <div className="follow">
+            <p>Follow me</p>
+            <div className="social-links">
+              <FaLinkedin />
+              <FaGithubSquare />
+              <FaTwitterSquare />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="footer-second">
+        <form action="#" method="post">
+          <input type="text" placeholder='Your name' />
+          <input type="email" placeholder='Your email address' />
+          <input type="text" placeholder='Your budget (optional)' />
+          <input type="text" placeholder='Your project description' className='description'/>
+          <div className="btn-wrapper">
+          <button>SUBMIT</button>
+          </div>
+        </form>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
